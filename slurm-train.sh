7b625a4b2bd1d1df575585879676eb4a08c4f629
Abdul BasitTonmoy <abdulbasittonmoy@gmail.com>
2025-12-12 12:15:07 -0500

    crash report

--- slurm-train.sh
+++ slurm-train.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=test1
+#SBATCH --job-name=vessel_train
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --output=test1%j.out
+#SBATCH --output=vessel_train_%j.out
 
-
-
-source /home/jhub/jhub-venv/bin/activate
+eval "$(micromamba shell hook --shell=bash)"
+micromamba activate tfenv
 
 export LD_LIBRARY_PATH=/home/jhub/jhub-venv/lib/python3.10/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/home/jhub/jhub-venv/lib/python3.10/site-packages/nvidia/cublas/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/home/jhub/jhub-venv/lib/python3.10/site-packages/nvidia/cusolver/lib:$LD_LIBRARY_PATH
 
-
-
-jupyter execute test1.ipynb
+jupyter execute new_main.ipynb
